5fb78877ad7a55bdfd774e8ef53f87c8d8e294b9
Giang <giangb2014651@student.ctu.edu.vn>
2024-01-04 17:08:58 +0700

    Cai dat xu ly loi

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express=require("express");
 const cors=require("cors");
 const contactsRouter=require("./app/routes/contact.route");
+const ApiError=require("./app/api-error");
 
 const app=express();
 
@@ -10,5 +11,15 @@ app.get("/",(req,res) => {
     res.json({message: "Welcome to contact book application."});
 });
 app.use("/api/contacts",contactsRouter);
+
+app.use((req,res,next) => {
+    return next(new ApiError(404, "Resource not found"));
+});
+
+app.use((err,req,res,next) => {
+    return res.status(err.statusCode || 500).json({
+        message: err.message || "Internal Server Error".at,
+    });
+});
 
 module.exports=app;
